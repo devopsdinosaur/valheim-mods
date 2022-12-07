01975bdd305167b7c4d2ad464df1c0f4292405e1
devopsdinosaur <devopsdinosaur@gmail.com>
2022-12-07 08:48:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# valheim-mods
+This repo contains the source code and Visual Studio project files for building my Valheim mods.  All of the built mods in this repo are available on Nexus Mods here: https://www.nexusmods.com/valheim/mods/
---
+++ unbreakable_tools/Plugin.cs
@@ -0,0 +1,45 @@
+﻿
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+
+[BepInPlugin("devopsdinosaur.valheim.unbreakable_tools", "Unbreakable Tools", "0.0.1")]
+public class Plugin : BaseUnityPlugin {
+
+	private Harmony m_harmony = new Harmony("devopsdinosaur.valheim.unbreakable_tools");
+	public static ManualLogSource logger;
+
+	public Plugin() {
+	}
+
+	private void Awake() {
+		Plugin.logger = this.Logger;
+		logger.LogInfo((object) "devopsdinosaur.valheim.unbreakable_tools v0.0.1 loaded.");
+		this.m_harmony.PatchAll();
+	}
+
+	private void Start() {
+
+	}
+
+	[HarmonyPatch(typeof(Player), "FixedUpdate")]
+	class HarmonyPatch_Player_FixedUpdate {
+
+		const float CHECK_FREQUENCY = 1.0f;
+		static float m_elapsed = 0f;
+
+		private static void Postfix(ref Player __instance, ref Inventory ___m_inventory) {
+			if (__instance != Player.m_localPlayer || (m_elapsed += Time.fixedDeltaTime) < CHECK_FREQUENCY) {
+				return;
+			}
+			m_elapsed = 0f;
+			foreach (ItemDrop.ItemData item in ___m_inventory.GetAllItems()) {
+				if (item.IsEquipable()) {
+					item.m_durability = item.GetMaxDurability();
+				}
+			}
+		}
+	}
+}
