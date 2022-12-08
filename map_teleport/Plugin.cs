c7b23acc65cae747cf6c1591726208ed1605861b
devopsdinosaur <devopsdinosaur@gmail.com>
2022-12-08 11:51:52 -0500

    Map teleport now working.  Added no_death_animations.  Working on indoor_campfire

--- map_teleport/Plugin.cs
+++ map_teleport/Plugin.cs
@@ -5,6 +5,7 @@ using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Reflection;
+using System;
 
 
 [BepInPlugin("devopsdinosaur.valheim.map_teleport", "Map Teleport", "0.0.1")]
@@ -23,20 +24,34 @@ public class Plugin : BaseUnityPlugin {
 	}
 
 	private void Start() {
-
 	}
 
 	[HarmonyPatch(typeof(Minimap), "OnMapMiddleClick")]
 	class HarmonyPatch_Minimap_OnMap_MiddleClick {
 
 		private static bool Prefix(ref Minimap __instance) {
-			Vector3 position = __instance.
+			Vector3 position = (Vector3) __instance.
 				GetType().
 				GetTypeInfo().
 				GetDeclaredMethod("ScreenToWorldPoint").
-				Invoke(__instance, (object[]) new Vector3[] {Input.mousePosition});
-
+				Invoke(__instance, new object[] {Input.mousePosition});
+			Chat.instance.SendPing(position);
+			if (Input.GetKey(KeyCode.LeftControl)) {
+				Vector3 vector = new Vector3(position.x, Player.m_localPlayer.transform.position.y, position.z);
+				Heightmap.GetHeight(vector, out var height);
+				vector.y = Math.Max(0f, height);
+				Player.m_localPlayer.TeleportTo(vector, Player.m_localPlayer.transform.rotation, distantTeleport: true);
+			}
 			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(Player), "TeleportTo")]
+	class HarmonyPatch_Player_TeleportTo {
+
+		private static void Postfix(ref float ___m_teleportTimer, ref bool ___m_distantTeleport) {
+			___m_teleportTimer = 8f;
+			___m_distantTeleport = false;
 		}
 	}
 
---
+++ no_death_animations/Plugin.cs
@@ -0,0 +1,38 @@
+﻿
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+
+[BepInPlugin("devopsdinosaur.valheim.no_death_animations", "No Death Animations", "0.0.1")]
+public class Plugin : BaseUnityPlugin {
+
+	private Harmony m_harmony = new Harmony("devopsdinosaur.valheim.no_death_animations");
+	public static ManualLogSource logger;
+
+	public Plugin() {
+	}
+
+	private void Awake() {
+		Plugin.logger = this.Logger;
+		logger.LogInfo((object) "devopsdinosaur.valheim.no_death_animations v0.0.1 loaded.");
+		this.m_harmony.PatchAll();
+	}
+
+	private void Start() {
+	}
+
+	[HarmonyPatch(typeof(Ragdoll), "Setup")]
+	class HarmonyPatch_Ragdoll_Setup {
+
+		private static void Postfix(ref Ragdoll __instance) {
+			__instance.
+				GetType().
+				GetTypeInfo().
+				GetDeclaredMethod("DestroyNow").
+				Invoke(__instance, new object[] { });
+		}
+	}
+
+}
