43c6f0f184ae649b695e71df02d401ef81d7c24e
devopsdinosaur <devopsdinosaur@gmail.com>
2022-12-15 13:56:31 -0500

    Minor improvements to death stuff

--- keep_items_on_death/Plugin.cs
+++ keep_items_on_death/Plugin.cs
@@ -3,6 +3,7 @@ using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
+using System.Collections.Generic;
 
 
 [BepInPlugin("devopsdinosaur.valheim.keep_items_on_death", "Keep Items on Death", "0.0.1")]
@@ -20,6 +21,25 @@ public class Plugin : BaseUnityPlugin {
 		this.m_harmony.PatchAll();
 	}
 
+	/*
+	[HarmonyPatch(typeof(Player), "UseHotbarItem")]
+	class HarmonyPatch_Player_UseHotbarItem {
+
+		Not currently working...
+
+		private static bool Prefix(ref Player __instance, int index, ref Inventory ___m_inventory) {
+			ItemDrop.ItemData item = ___m_inventory.GetItemAt(index, 0);
+			if (item != null) {
+				logger.LogInfo(item.ToString());
+			}
+			if (item != null && item.m_equiped) {
+				return false;
+			}
+			return true;
+		}
+	}
+	*/
+
 	[HarmonyPatch(typeof(Player), "UnequipDeathDropItems")]
 	class HarmonyPatch_Player_UnequipDeathDropItems {
 
@@ -28,8 +48,8 @@ public class Plugin : BaseUnityPlugin {
 		}
 	}
 
-	[HarmonyPatch(typeof(Player), "CreateTombstone")]
-	class HarmonyPatch_Player_CreateTombstone {
+	[HarmonyPatch(typeof(Player), "CreateTombStone")]
+	class HarmonyPatch_Player_CreateTombStone {
 
 		private static bool Prefix() {
 			return false;
@@ -44,4 +64,28 @@ public class Plugin : BaseUnityPlugin {
 		}
 	}
 
+	[HarmonyPatch(typeof(Player), "OnDeath")]
+	class HarmonyPatch_Player_OnDeath {
+
+		static List<Player.Food> foods;
+
+		private static bool Prefix(ref List<Player.Food> ___m_foods) {
+			foods = new List<Player.Food>(___m_foods);
+			return true;
+		}
+
+		private static void Postfix(ref List<Player.Food> ___m_foods) {
+			___m_foods = foods;
+		}
+	}
+
+	[HarmonyPatch(typeof(Game), "RequestRespawn")]
+	class HarmonyPatch_Game_RequestRespawn {
+
+		private static bool Prefix(ref Game __instance) {
+			__instance.CancelInvoke("_RequestRespawn");
+			__instance.Invoke("_RequestRespawn", 0);
+			return false;
+		}
+	}
 }
